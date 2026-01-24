f711a8bc7df771f72ff59bc4029bb797ae8aae61
Aitan Martinez <prisimrobotics@gmail.com>
2026-01-23 20:49:26 -0600

    Finished climber

--- src/main/java/frc/robot/Subsystems/Climber/ClimberIOReal.java
+++ src/main/java/frc/robot/Subsystems/Climber/ClimberIOReal.java
@@ -1,6 +1,50 @@
 package frc.robot.Subsystems.Climber;
 
+import static edu.wpi.first.units.Units.Rotations;
 
-public class ClimberIOReal {
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.units.measure.Angle;
+import org.littletonrobotics.junction.Logger;
 
+public class ClimberIOReal implements ClimberIO {
+
+	protected TalonFX leftMotor;
+	protected TalonFX rightMotor;
+	protected Angle positionSetpoint;
+	protected PIDController pid;
+
+	public ClimberIOReal() {
+		leftMotor = new TalonFX(ClimberConstants.LEFT_CLIMBER_MOTOR_ID);
+		rightMotor = new TalonFX(ClimberConstants.RIGHT_CLIMBER_MOTOR_ID);
+		rightMotor.setControl(new Follower(leftMotor.getDeviceID(), MotorAlignmentValue.Aligned));
+
+		pid = ClimberConstants.CLIMB_PID.get();
+		positionSetpoint = ClimberConstants.IDLE_SETPOINT;
+	}
+
+	@Override
+	public void logOutputs(ClimberIOOutputs outputs) {
+		outputs.leftPosition = leftMotor.getPosition().getValue();
+		outputs.rightPosition = rightMotor.getPosition().getValue();
+		outputs.setpoint = positionSetpoint;
+
+		Logger.recordOutput(ClimberConstants.SUBSYSTEM_NAME + "/LeftPositionRot", outputs.leftPosition.in(Rotations));
+		Logger.recordOutput(ClimberConstants.SUBSYSTEM_NAME + "/RightPositionRot", outputs.rightPosition.in(Rotations));
+		Logger.recordOutput(ClimberConstants.SUBSYSTEM_NAME + "/SetpointRot", outputs.setpoint.in(Rotations));
+	}
+
+	@Override
+	public void setPosition(Angle position) {
+		this.positionSetpoint = position;
+		leftMotor.set(pid.calculate(leftMotor.getPosition().getValue().in(Rotations), positionSetpoint.in(Rotations)));
+	}
+
+	@Override
+	public boolean atPositionSetpoint() {
+		double currentRot = leftMotor.getPosition().getValue().in(Rotations);
+		return Math.abs(currentRot - positionSetpoint.in(Rotations)) < ClimberConstants.CLIMB_POSITION_TOLERANCE.in(Rotations);
+	}
 }
--- src/main/java/frc/robot/Subsystems/Climber/ClimberIOSim.java
+++ src/main/java/frc/robot/Subsystems/Climber/ClimberIOSim.java
@@ -1,5 +1,78 @@
 package frc.robot.Subsystems.Climber;
 
-public class ClimberIOSim {
-    
+import static edu.wpi.first.units.Units.Rotations;
+
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.sim.TalonFXSimState;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.ElevatorSim;
+import edu.wpi.first.units.measure.Angle;
+import org.littletonrobotics.junction.Logger;
+import frc.robot.GlobalConstants;
+
+public class ClimberIOSim extends ClimberIOReal {
+	protected TalonFXSimState leftMotorSim;
+	protected TalonFXSimState rightMotorSim;
+	protected ElevatorSim climbSim;
+	protected Angle positionSetpoint;
+
+	public ClimberIOSim() {
+		leftMotor = new TalonFX(ClimberConstants.LEFT_CLIMBER_MOTOR_ID);
+		rightMotor = new TalonFX(ClimberConstants.RIGHT_CLIMBER_MOTOR_ID);
+		rightMotor.setControl(new Follower(leftMotor.getDeviceID(), MotorAlignmentValue.Aligned));
+
+		leftMotorSim = new TalonFXSimState(leftMotor);
+		rightMotorSim = new TalonFXSimState(rightMotor);
+		climbSim = new ElevatorSim(
+			LinearSystemId.createElevatorSystem(
+				DCMotor.getFalcon500(1),
+				ClimberConstants.CLIMBER_MASS,
+				ClimberConstants.CLIMBER_RADIUS,
+                ClimberConstants.CLIMBER_GEARING
+			),
+			DCMotor.getFalcon500(1),
+			ClimberConstants.START_HEIGHT,
+			ClimberConstants.END_HEIGHT,
+			true,
+            ClimberConstants.START_HEIGHT
+		);
+
+		positionSetpoint = ClimberConstants.IDLE_SETPOINT;
+	}
+
+	@Override
+	public void logOutputs(ClimberIOOutputs outputs) {
+		climbSim.update(GlobalConstants.SIMULATION_PERIOD);
+
+		leftMotorSim.setRawRotorPosition(climbSim.getPositionMeters());
+		leftMotorSim.setRotorVelocity(climbSim.getVelocityMetersPerSecond());
+		rightMotorSim.setRawRotorPosition(climbSim.getPositionMeters());
+		rightMotorSim.setRotorVelocity(climbSim.getVelocityMetersPerSecond());
+
+		outputs.leftPosition = leftMotor.getPosition().getValue();
+		outputs.rightPosition = rightMotor.getPosition().getValue();
+		outputs.setpoint = positionSetpoint;
+
+		Logger.recordOutput(ClimberConstants.SUBSYSTEM_NAME + "/SimLeftRot", outputs.leftPosition.in(Rotations));
+		Logger.recordOutput(ClimberConstants.SUBSYSTEM_NAME + "/SimRightRot", outputs.rightPosition.in(Rotations));
+		Logger.recordOutput(ClimberConstants.SUBSYSTEM_NAME + "/SimSetpointRot", outputs.setpoint.in(Rotations));
+	}
+
+	@Override
+	public void setPosition(Angle position) {
+		this.positionSetpoint = position;
+		double currentRot = leftMotor.getPosition().getValue().in(Rotations);
+		double error = position.in(Rotations) - currentRot;
+		double effort = Math.max(-1.0, Math.min(1.0, 5.0 * error));
+		climbSim.setInputVoltage(effort * 12.0);
+	}
+
+	@Override
+	public boolean atPositionSetpoint() {
+		double currentRot = leftMotor.getPosition().getValue().in(Rotations);
+		return Math.abs(currentRot - positionSetpoint.in(Rotations)) < ClimberConstants.CLIMB_POSITION_TOLERANCE.in(Rotations);
+	}
 }
