07a502009953a0d449b3eb6177d05458fae4735d
Aitan Martinez <prisimrobotics@gmail.com>
2026-01-24 22:02:24 -0600

    Minor fixes and adding visualize states workflow (removed forcing connections to idle)

--- src/main/java/frc/robot/Subsystems/Manager/Manager.java
+++ src/main/java/frc/robot/Subsystems/Manager/Manager.java
@@ -115,5 +115,37 @@ public class Manager extends Subsystem<ManagerStates> {
         return true; // TODO: implement this idk how
     }
 
+    @Override
+	public void periodic() {
+		super.periodic();
+		clearButtonPressCache();
+	}
+
+    public void clearButtonPressCache() {
+		DRIVER_CONTROLLER.getAButtonPressed();
+		DRIVER_CONTROLLER.getBButtonPressed();
+		DRIVER_CONTROLLER.getXButtonPressed();
+		DRIVER_CONTROLLER.getYButtonPressed();
+		DRIVER_CONTROLLER.getBackButtonPressed();
+		DRIVER_CONTROLLER.getStartButtonPressed();
+		DRIVER_CONTROLLER.getLeftBumperButtonPressed();
+		DRIVER_CONTROLLER.getRightBumperButtonPressed();
+		DRIVER_CONTROLLER.getLeftStickButtonPressed();
+		DRIVER_CONTROLLER.getRightStickButtonPressed();
+        OPERATOR_CONTROLLER.getAButtonPressed();
+        OPERATOR_CONTROLLER.getBButtonPressed();
+        OPERATOR_CONTROLLER.getXButtonPressed();
+        OPERATOR_CONTROLLER.getYButtonPressed();
+        OPERATOR_CONTROLLER.getBackButtonPressed();
+        OPERATOR_CONTROLLER.getStartButtonPressed();
+        OPERATOR_CONTROLLER.getLeftBumperButtonPressed();
+        OPERATOR_CONTROLLER.getRightBumperButtonPressed();
+        OPERATOR_CONTROLLER.getLeftStickButtonPressed();
+        OPERATOR_CONTROLLER.getRightStickButtonPressed();
+    }
+
+	@Override
+	protected void stateExit() {}
+
 
 }
