ca7f6c66a636c6e81f5b78f76a6f3878937bffa5
Otto <grentomato@gmail.com>
2026-02-06 17:00:54 -0600

    Add seperate Shooting Command

--- src/main/java/frc/robot/Commands/AngleAndShootCommand.java
+++ src/main/java/frc/robot/Commands/AngleAndShootCommand.java
@@ -1,3 +1,29 @@
 package frc.robot.Commands;
 
-public class AngleAndShootCommand {}
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Robot;
+import frc.robot.Subsystems.Manager.ManagerStates;
+
+public class AngleAndShootCommand {
+    // needed because shooting is a continuous action that you cannot be driving whilst 
+    // (we can investigate SOTM but for now this is regular)
+    
+    public class Shooting extends Command {
+    Robot robot;
+
+    public Shooting(Robot robot) {
+        this.robot = robot;
+    }
+
+    @Override
+    public void initialize() {
+        robot.getManager().setState(ManagerStates.SHOOTING_HUB);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return robot.getManager().getState() == ManagerStates.IDLE;
+    }
+}
+
+}
