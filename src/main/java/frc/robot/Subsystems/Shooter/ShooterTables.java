2081f5a1b6d1012d5703e543707614ce1e02a1ff
Aitan Martinez <prisimrobotics@gmail.com>
2026-03-16 00:47:57 -0500

    Data points + Hood fix hopefully

--- src/main/java/frc/robot/Subsystems/Shooter/ShooterTables.java
+++ src/main/java/frc/robot/Subsystems/Shooter/ShooterTables.java
@@ -6,13 +6,21 @@ public class ShooterTables {
 
 	public static InterpolatingDoubleTreeMap ALLIANCE_TABLE = new InterpolatingDoubleTreeMap();
 
-	static { // DISTANCE, ANGLE
-		ALLIANCE_TABLE.put(0.0, 0.0);
+	static { // DISTANCE (M), ANGLE TODO: THESE ARE CURRENTLY IN EDGE TO EDGE. MUST CONVERT TO SHOOTER TO HUB CENTER
+		ALLIANCE_TABLE.put(6.0, 32.0); // Here so we have better bounds of table. Do not include if using regressions
+		ALLIANCE_TABLE.put(5.7404, 30.0);
+		ALLIANCE_TABLE.put(5.2959, 25.0);
+		ALLIANCE_TABLE.put(4.7879, 20.0);
+		ALLIANCE_TABLE.put(4.29895, 15.0);
+		ALLIANCE_TABLE.put(3.3147, 10.0);
+		ALLIANCE_TABLE.put(1.6764, 5.0);
+		ALLIANCE_TABLE.put(0.3683, 0.0);
+		ALLIANCE_TABLE.put(0.0, 0.0); // Here so we have better bounds of table. Do not include if using regressions
 	}
+	public static InterpolatingDoubleTreeMap SHUTTLING_TABLE = ALLIANCE_TABLE; 
+	//public static InterpolatingDoubleTreeMap SHUTTLING_TABLE = new InterpolatingDoubleTreeMap();
 
-	public static InterpolatingDoubleTreeMap SHUTTLING_TABLE = new InterpolatingDoubleTreeMap();
-
-	static {
-		SHUTTLING_TABLE.put(0.0, 0.0);
-	}
+	// static {
+	// 	SHUTTLING_TABLE.put(0.0, 0.0);
+	// }
 }
