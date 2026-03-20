7428d980c57dcf8536cbe813a517c10a6c647941
PotmanNob <Potmannob@outlook.com>
2026-03-20 10:28:08 -0500

    Logging for ShooterMath

--- src/main/java/frc/robot/Subsystems/Shooter/ShooterMath.java
+++ src/main/java/frc/robot/Subsystems/Shooter/ShooterMath.java
@@ -4,6 +4,8 @@ import static edu.wpi.first.units.Units.Degrees;
 import static frc.robot.FieldConstants.*;
 import static frc.robot.GlobalConstants.ROBOT_TO_SHOOTER_2D;
 
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.units.measure.Angle;
 import frc.robot.Robot;
@@ -11,6 +13,11 @@ import frc.robot.Robot;
 public class ShooterMath {
 
 	public static Angle solveHubShot(Pose2d currentPose) {
+		Logger.recordOutput("Shooter/Math/Target", Robot.isRedAlliance ? RED_HUB_POSE : BLUE_HUB_POSE);
+		Logger.recordOutput("Shooter/Math/Shooter", currentPose.transformBy(ROBOT_TO_SHOOTER_2D));
+		Logger.recordOutput("Shooter/Math/Translation", Robot.isRedAlliance ? RED_HUB_POSE.relativeTo(currentPose.transformBy(ROBOT_TO_SHOOTER_2D)) : BLUE_HUB_POSE.relativeTo(currentPose.transformBy(ROBOT_TO_SHOOTER_2D)));
+		Logger.recordOutput("Shooter/Math/Distance", Robot.isRedAlliance ? RED_HUB_POSE.relativeTo(currentPose.transformBy(ROBOT_TO_SHOOTER_2D)).getTranslation().getNorm() : BLUE_HUB_POSE.relativeTo(currentPose.transformBy(ROBOT_TO_SHOOTER_2D)).getTranslation().getNorm());
+
 		return Degrees.of(
 			Robot.isRedAlliance ? ShooterTables.ALLIANCE_TABLE.get(RED_HUB_POSE.relativeTo(currentPose.transformBy(ROBOT_TO_SHOOTER_2D)).getTranslation().getNorm()) : ShooterTables.ALLIANCE_TABLE.get(BLUE_HUB_POSE.relativeTo(currentPose.transformBy(ROBOT_TO_SHOOTER_2D)).getTranslation().getNorm())
 		);
