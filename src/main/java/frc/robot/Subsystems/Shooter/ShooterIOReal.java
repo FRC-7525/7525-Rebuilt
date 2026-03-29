7a337aa0c811f88f8c72cd6ec831f3e138822670
Aitan Martinez <161392373+Tortuga-AM@users.noreply.github.com>
2026-03-29 12:28:49 -0400

    tuning version (#41)

--- src/main/java/frc/robot/Subsystems/Shooter/ShooterIOReal.java
+++ src/main/java/frc/robot/Subsystems/Shooter/ShooterIOReal.java
@@ -74,17 +74,13 @@ public class ShooterIOReal implements ShooterIO {
 		outputs.hoodCurrent = hoodMotor.getSupplyCurrent().getValueAsDouble();
 		outputs.leftWheelCurrent = leftMotor.getSupplyCurrent().getValueAsDouble();
 		outputs.rightWheelCurrent = rightMotor.getSupplyCurrent().getValueAsDouble();
-
-		SmartDashboard.putData("ShooterWheelPID", wheelPID);
-		wheelFeedforward.setKv(SmartDashboard.getNumber("ShooterFeedforwardKv", wheelFeedforward.getKv()));
-		SmartDashboard.putNumber("ShooterFeedforwardKv", wheelFeedforward.getKv());
-		SmartDashboard.putData("ShooterHoodPID", hoodPID);
-		SmartDashboard.putData("ShooterHoodDownPID", hoodDownPID);
 	}
 
 	@Override
 	public void setWheelVelocity(AngularVelocity velocity) {
 		wheelSetpoint = velocity;
+		velocity = RotationsPerSecond.of(SmartDashboard.getNumber("SHOOTER SPEED", 0));
+		SmartDashboard.putNumber("SHOOTER SPEED", velocity.in(RotationsPerSecond));
 		if (velocity.in(RotationsPerSecond) == 0) {
 			leftMotor.stopMotor();
 			return;
@@ -95,8 +91,10 @@ public class ShooterIOReal implements ShooterIO {
 	@Override
 	public void setHoodAngle(Angle angle) {
 		hoodSetpoint = angle;
-		if (angle.in(Degrees) != 0) {
-			hoodMotor.set(hoodPID.calculate(hoodMotor.getPosition().getValue().div(HOOD_GEARING).in(Degrees), hoodSetpoint.in(Degrees)));
+		double anglTest = SmartDashboard.getNumber("HOOD ANGLE", 0);
+		SmartDashboard.putNumber("HOOD ANGLE", anglTest);
+		if (anglTest != 0) {
+			hoodMotor.set(hoodPID.calculate(hoodMotor.getPosition().getValue().div(HOOD_GEARING).in(Degrees), anglTest));
 		} else {
 			if (!limitSwitch.get() && hoodMotor.getPosition().getValue().in(Degrees) != 0) {
 				hoodMotor.set(0);
@@ -105,7 +103,7 @@ public class ShooterIOReal implements ShooterIO {
 				hoodDownPID.reset();
 				return;
 			}
-			hoodMotor.set(hoodDownPID.calculate(hoodMotor.getPosition().getValue().div(HOOD_GEARING).in(Degrees), hoodSetpoint.in(Degrees)));
+			hoodMotor.set(hoodDownPID.calculate(hoodMotor.getPosition().getValue().div(HOOD_GEARING).in(Degrees), anglTest));
 		}
 	}
 
